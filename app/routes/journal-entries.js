dac28a91150a1f578ea339b0abbe24d676df5fc9
Drew Keller <drew@eightycolumns.com>
2017-09-24 15:53:27 -0400

    Extract initializeLocalStorage()

--- app/routes/journal-entries.js
+++ app/routes/journal-entries.js
@@ -1,26 +1,9 @@
 import Ember from 'ember';
 
-const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
-                   'sed do eiusmod tempor incididunt ut labore et dolore ' +
-                   'magna aliqua. Ut enim ad minim veniam, quis nostrud ' +
-                   'exercitation ullamco laboris nisi ut aliquip ex ea ' +
-                   'commodo consequat. Duis aute irure dolor in ' +
-                   'reprehenderit in voluptate velit esse cillum dolore eu ' +
-                   'fugiat nulla pariatur. Excepteur sint occaecat cupidatat ' +
-                   'non proident, sunt in culpa qui officia deserunt mollit ' +
-                   'anim id est laborum.';
-
 export default Ember.Route.extend({
   model() {
     if (!localStorageIsInitialized()) {
-      const journalEntries = [
-        {title: 'Journal Entry #1', date: '21 September 2017', text: loremIpsum},
-        {title: 'Journal Entry #2', date: '22 September 2017', text: loremIpsum},
-        {title: 'Journal Entry #3', date: '22 September 2017', text: loremIpsum},
-        {title: 'Journal Entry #4', date: '23 September 2017', text: loremIpsum}
-      ];
-
-      localStorage.setItem('journalEntries', JSON.stringify(journalEntries));
+      initializeLocalStorage();
     }
 
     return JSON.parse(localStorage.getItem('journalEntries'));
@@ -29,4 +12,25 @@ export default Ember.Route.extend({
 
 function localStorageIsInitialized() {
   return localStorage.getItem('journalEntries');
+}
+
+function initializeLocalStorage() {
+  const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing ' +
+                     'elit sed do eiusmod tempor incididunt ut labore et ' +
+                     'dolore magna aliqua. Ut enim ad minim veniam, quis ' +
+                     'nostrud exercitation ullamco laboris nisi ut aliquip ' +
+                     'ex ea commodo consequat. Duis aute irure dolor in ' +
+                     'reprehenderit in voluptate velit esse cillum dolore eu ' +
+                     'fugiat nulla pariatur. Excepteur sint occaecat ' +
+                     'cupidatat non proident, sunt in culpa qui officia ' +
+                     'deserunt mollit anim id est laborum.';
+
+  const journalEntries = [
+    {title: 'Journal Entry #1', date: '21 September 2017', text: loremIpsum},
+    {title: 'Journal Entry #2', date: '22 September 2017', text: loremIpsum},
+    {title: 'Journal Entry #3', date: '22 September 2017', text: loremIpsum},
+    {title: 'Journal Entry #4', date: '23 September 2017', text: loremIpsum}
+  ];
+
+  localStorage.setItem('journalEntries', JSON.stringify(journalEntries));
 }
