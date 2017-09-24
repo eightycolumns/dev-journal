80067a75af76b3ff411cd1ff6020b9e28c2b765b
Drew Keller <drew@eightycolumns.com>
2017-09-24 17:49:03 -0400

    Add setDate() action

--- app/components/new-journal-entry-form.js
+++ app/components/new-journal-entry-form.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  date: new Date()
+  date: new Date(),
+
+  actions: {
+    setDate(date) {
+      this.set('date', date);
+    }
+  }
 });
