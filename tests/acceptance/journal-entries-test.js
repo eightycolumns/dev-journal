8a254757b7b2d07d0db31041848a640067f58834
Drew Keller <drew@eightycolumns.com>
2017-09-25 16:28:16 -0400

    Test deletion of entry

--- tests/acceptance/journal-entries-test.js
+++ tests/acceptance/journal-entries-test.js
@@ -14,3 +14,31 @@ test('it is accessible via /', assert => {
   visit('/');
   andThen(() => assert.equal(currentURL(), '/journal-entries'));
 });
+
+test('it allows the user to delete a journal entry', assert => {
+  assert.expect(2);
+
+  visit('/new-journal-entry');
+
+  fillIn('#title', 'Journal Entry Title');
+  fillIn('#text', 'Journal entry...');
+
+  click('.publish');
+
+  visit('/new-journal-entry');
+
+  fillIn('#title', 'Another Journal Entry Title');
+  fillIn('#text', 'Another journal entry...');
+
+  click('.publish');
+
+  click('.journal-entry .delete');
+
+  andThen(() => {
+    const titleField = document.querySelector('.journal-entry h3');
+    assert.equal(titleField.textContent, 'Journal Entry Title');
+
+    const textField = document.querySelector('.journal-entry .text');
+    assert.equal(textField.textContent, 'Journal entry...');
+  });
+});
