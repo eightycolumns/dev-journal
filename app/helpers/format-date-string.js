c03ec8c83385ed164c7356c457dbb2b7277138ad
Drew Keller <drew@eightycolumns.com>
2017-09-24 20:30:07 -0400

    Format date strings for display

---
+++ app/helpers/format-date-string.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+export function formatDateString([dateString]) {
+  const date = new Date(dateString);
+
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  return `${day} ${month} ${year}`;
+}
+
+export default Ember.Helper.helper(formatDateString);
---
+++ tests/unit/helpers/format-date-string-test.js
@@ -0,0 +1,9 @@
+import { formatDateString } from 'dev-journal/helpers/format-date-string';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | format date string');
+
+test('it formats a date string for display', assert => {
+  const dateString = '2017-09-22T04:00:00.000Z';
+  assert.equal(formatDateString([dateString]), '22 September 2017');
+});
