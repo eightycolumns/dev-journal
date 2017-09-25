93d5d9d6977959e11c08370ca6140e2a97603f0a
Drew Keller <drew@eightycolumns.com>
2017-09-25 16:13:58 -0400

    Test creation of new entry

--- tests/acceptance/journal-entries-test.js
+++ tests/acceptance/journal-entries-test.js
@@ -3,12 +3,14 @@ import { test } from 'qunit';
 
 moduleForAcceptance('Acceptance | journal entries');
 
-test('it is accessible via /journal-entries', (assert) => {
+test('it is accessible via /journal-entries', assert => {
+  assert.expect(1);
   visit('/journal-entries');
   andThen(() => assert.equal(currentURL(), '/journal-entries'));
 });
 
-test('it is accessible via /', (assert) => {
+test('it is accessible via /', assert => {
+  assert.expect(1);
   visit('/');
   andThen(() => assert.equal(currentURL(), '/journal-entries'));
 });
--- tests/acceptance/new-journal-entry-test.js
+++ tests/acceptance/new-journal-entry-test.js
@@ -3,7 +3,27 @@ import { test } from 'qunit';
 
 moduleForAcceptance('Acceptance | new journal entry');
 
-test('it is accessible via /new-journal-entry', (assert) => {
+test('it is accessible via /new-journal-entry', assert => {
+  assert.expect(1);
   visit('/new-journal-entry');
   andThen(() => assert.equal(currentURL(), '/new-journal-entry'));
+});
+
+test('it allows the user to create a new journal entry', assert => {
+  assert.expect(2);
+
+  visit('/new-journal-entry');
+
+  fillIn('#title', 'Journal Entry Title');
+  fillIn('#text', 'Journal entry...');
+
+  click('.publish');
+
+  andThen(() => {
+    const titleField = document.querySelector('.journal-entry h3');
+    assert.equal(titleField.textContent, 'Journal Entry Title');
+
+    const textField = document.querySelector('.journal-entry .text');
+    assert.equal(textField.textContent, 'Journal entry...');
+  });
 });
