5a6a3e5ffc41d28bb0b8fdf4d631137c1841c508
Drew Keller <drew@eightycolumns.com>
2017-09-26 08:48:17 -0400

    Revise initial journal entries

--- app/routes/journal-entries.js
+++ app/routes/journal-entries.js
@@ -32,31 +32,31 @@ function initializeLocalStorage() {
   const journalEntries = [
     {
       id: 1,
-      title: 'Journal Entry #1',
-      date: '21 September 2017',
+      title: "Isn't Tomster Awesome?",
+      date: '23 September 2017',
       mood: 'Happy!',
-      text: 'Today I started learning Ember. It\'s described as "a framework for creating ambitious web applications", and it seems pretty neat. But you know why I\'m REALLY excited about Ember? Its mascot Tomster is a hamster, just like me! How cool is that?'
+      text: "Today I started learning Ember. It's described as \"a framework for creating ambitious web applications\", and it seems pretty neat. But you know why I'm REALLY excited about Ember? Its mascot Tomster is a hamster, just like me! How cool is that?"
     },
     {
       id: 2,
-      title: 'Journal Entry #2',
-      date: '22 September 2017',
+      title: 'What an Ecosystem!',
+      date: '24 September 2017',
       mood: 'Happy!',
-      text: 'The Ember ecosystem is kind of awesome. Ember\'s website has lots of guides and tutorials, the Ember CLI tool generates skeletal applications in seconds, and WOW there are tons of addons! It\'s no wonder people like this framework.'
+      text: "The Ember ecosystem is kind of awesome. Ember's website has lots of guides and tutorials, the Ember CLI tool generates skeletal applications in seconds, and WOW there are tons of addons! It's no wonder people like this framework."
     },
     {
       id: 3,
-      title: 'Journal Entry #3',
-      date: '22 September 2017',
+      title: 'Learning QUnit',
+      date: '24 September 2017',
       mood: 'Meh...',
-      text: 'Learning to test Ember with QUnit has been a bit of an adjustment. In the past I\'ve always used Jasmine for JavaScript. But I think I\'m getting the hang of it. Just need more practice!'
+      text: "Learning to test Ember with QUnit has been a bit of an adjustment. In the past I've only used Jasmine for JavaScript. But I'm starting to get the hang of it. Just need more practice!"
     },
     {
       id: 4,
-      title: 'Journal Entry #4',
-      date: '23 September 2017',
+      title: 'My First Ember App',
+      date: '25 September 2017',
       mood: 'Happy!',
-      text: 'Yay, my app works! Pretty good for a hamster, right? Hehe, just kidding. It\'s alright, but I\'ve really only scratched the surface. It\'s been a treat getting to know Ember this week, but I still have a long way to go.'
+      text: "Yay, my app works! Pretty good for a hamster, right? Hehe, just kidding. It's alright, but I've still got a long way to go. Getting to know Ember this week has been a treat, but I've really only scratched the surface."
     }
   ];
 
